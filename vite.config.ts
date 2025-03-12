5f2946db9ab92e82a3a860be36ad429064639261
Claudio Damasco <claudiodamasco@Mac-Studio-de-Claudio.local>
2025-03-11 23:13:19 -0300

    vite config ajuste

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          ui: ['@mui/material', '@mui/icons-material', '@emotion/react', '@emotion/styled'],
+          charts: ['chart.js', 'react-chartjs-2'],
+          social: ['@greatsumini/react-facebook-login', 'react-facebook-login', 'react-linkedin-login-oauth2', 'react-twitter-login']
+        }
+      }
+    }
+  }
 })
